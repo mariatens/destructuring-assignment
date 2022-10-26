93a304a00942c36d901d108ca40e033856e05c10
Maria <mtens17@gmail.com>
2022-10-26 11:52:32 +0100

    Add parameter destructuring to 1b

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,6 +1,10 @@
-function listFollowers(followerNames: string[]): string {
-  const [follower1, follower2, follower3] = followerNames
+// function listFollowers(followerNames: string[]): string {
+//   const [follower1, follower2] = followerNames
+//   return `Followed by ${follower1}, ${follower2} and others`;
+// }
+
+// w parameter destructuring
+function listFollowers([follower1, follower2]: string[]): string {
   return `Followed by ${follower1}, ${follower2} and others`;
 }
-
 export default listFollowers;
