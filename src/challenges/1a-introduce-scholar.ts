d20dcff663f274c43b249cf67eb9ed0172596e2a
Maria <mtens17@gmail.com>
2022-10-26 11:36:50 +0100

    Add 1a with and without destructuring

--- src/challenges/1a-introduce-scholar.ts
+++ src/challenges/1a-introduce-scholar.ts
@@ -3,11 +3,14 @@ interface Scholar {
   lastName: string;
   cohortNumber: number;
 }
-
+// wo object destructuring
+// function introduceScholar(scholar: Scholar): string {
+//   return `${scholar.firstName} ${scholar.lastName} is an Academy Scholar, on Cohort ${scholar.cohortNumber}.`;
+// }
+// w object destructuring 
 function introduceScholar(scholar: Scholar): string {
-  // complete function
-  // use object destructuring, for practice!
-  return "fails test right now";
+    const {firstName, lastName, cohortNumber} = scholar
+    return `${firstName} ${lastName} is an Academy Scholar, on Cohort ${cohortNumber}.`
 }
 
 export default introduceScholar;
